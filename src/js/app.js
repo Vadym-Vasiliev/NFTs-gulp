54de0fec73ba624a3a54adb5f8afc01a64141d32
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-24 10:58:34 +0300

    add fix form footer

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,6 @@ import * as flsFunctions from "./modules/functions.js";
 import "./modal.js";
 import "./menu.js";
 import "./form.js";
+import "./form-footer.js";
 
 flsFunctions.isWebp();
---
+++ src/js/form-footer.js
@@ -0,0 +1,40 @@
+let form = document.querySelector(".footer-list__form");
+let formInput = document.querySelector(".footer-list__input");
+let formInputEmail = document.querySelector(".footer-list__input-email");
+
+//перевірка пошти на символи
+function validateEmail(email) {
+  let symbols =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return symbols.test(String(email).toLowerCase());
+}
+
+//перевірка  на пусті поля
+form.onsubmit = function () {
+  let emailValue = formInputEmail.value;
+
+  //вертає ретюрн фолс якщо інпут пустий
+  let emptyInput = Array.from(formInput).filter((input) => input.value === "");
+
+  formInput.forEach(function (input) {
+    if (input.value === "") {
+      input.classList.add("error");
+    } else {
+      input.classList.remove("error");
+    }
+  });
+
+  if (emptyInput.length !== 0) {
+    console.log("Поля не заповнені!");
+    return false;
+  }
+
+  //перевірка на пошту
+  if (!validateEmail(emailValue)) {
+    console.log("Поле не заповнено!");
+    formInputEmail.classList.add("error");
+    return false;
+  } else {
+    formInputEmail.classList.remove("error");
+  }
+};
