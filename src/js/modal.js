f3c7d9f9eb9fb3d30803d61f935f64e00ac8811f
Vadym-Vasiliev <98331150+Vadym-Vasiliev@users.noreply.github.com>
2022-08-30 13:17:22 +0300

    add js close modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,9 +1,39 @@
+const modal = document.querySelector("[data-modal]");
 const openModalBtn = document.querySelector("[data-modal-open]");
 const closeModalBtn = document.querySelector("[data-modal-close]");
-const modal = document.querySelector("[data-modal]");
 
-openModalBtn.addEventListener("click", toggleModal);
-closeModalBtn.addEventListener("click", toggleModal);
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+
+function windowModalRemoveEventListener() {
+  window.removeEventListener("keydown", closeModalEscape);
+  modal.removeEventListener("click", closeModalClickBackdrop);
+}
+
+function openModal() {
+  window.addEventListener("keydown", closeModalEscape);
+  modal.addEventListener("click", closeModalClickBackdrop);
+  toggleModal();
+}
+
+function closeModal() {
+  windowModalRemoveEventListener();
+  toggleModal();
+}
+
+function closeModalEscape(e) {
+  if (e.code === "Escape") {
+    toggleModal();
+    windowModalRemoveEventListener();
+  }
+}
+
+function closeModalClickBackdrop(e) {
+  if (e.target === e.currentTarget) {
+    toggleModal();
+    windowModalRemoveEventListener();
+  }
+}
 
 function toggleModal() {
   document.body.classList.toggle("modal-open");
